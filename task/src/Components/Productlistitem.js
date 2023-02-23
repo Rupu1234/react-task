840169c53cd45c0c23a47c80d7bbf28b01868fff
Rupu1234 <rupu0812@gmail.com>
2023-02-23 14:36:42 +0530

    my project

--- task/src/Components/Productlistitem.js
+++ task/src/Components/Productlistitem.js
@@ -1,26 +1,26 @@
-// import React from 'react'
+import React from 'react'
 
-// export default function Productlistitem(props) {
-//   return (
-//     <div>Productlistitem
+export default function Productlistitem(props) {
+  return (
+    <div>Productlistitem
 
-// <div className='cart m-2 cursor-pointer'>
-//         <div className='mt-2'>
-//            <img src={props.thumbnail} height={150} width={180} alt={props.title}  className='image'/>
-//         </div>
+<div className='cart m-2 cursor-pointer'>
+        <div className='mt-2'>
+           <img src={props.thumbnail} height={150} width={180} alt={props.title}  className='image'/>
+        </div>
     
 
-//     <div className='card-body'>
-//         <h5 className='card-title'>{props.title}</h5>
-//         <h6 className='mt-2'>price:{`$${props.price}`}</h6>
-//         <h6 className='mt-2'>Discount:{props.discountPercentage}%</h6>
-//         <h6 className='mt-2'>Rating:{props.rating}</h6>
-//         <div>
-//             {props.stock >0?<button className='btn btn-success'>Available</button>:
-//             <button className='btn btn-outline-danger'>out of stock</button>}
-//         </div>
-//     </div>
-//    </div>  
-//     </div>
-//   )
-// }
+    <div className='card-body'>
+        <h5 className='card-title'>{props.title}</h5>
+        <h6 className='mt-2'>price:{`$${props.price}`}</h6>
+        <h6 className='mt-2'>Discount:{props.discountPercentage}%</h6>
+        <h6 className='mt-2'>Rating:{props.rating}</h6>
+        <div>
+            {props.stock >0?<button className='btn btn-success'>Available</button>:
+            <button className='btn btn-outline-danger'>out of stock</button>}
+        </div>
+    </div>
+   </div>  
+    </div>
+  )
+}
