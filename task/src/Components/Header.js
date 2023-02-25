a23f46b06b0eff5aabf8eb11d184353c36f249d3
Rupu1234 <rupu0812@gmail.com>
2023-02-25 19:53:31 +0530

    my project

--- task/src/Components/Header.js
+++ task/src/Components/Header.js
@@ -1,24 +1,33 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Header() {
-
-  const navigate=useNavigate()
+  const navigate = useNavigate();
 
   return (
-    <div className='bg-secondary p-3'>
-        <h3>My Projects</h3>
-        <div className='row justify-content-center pt-2 pb-1'>
-            <div className='col-sm-12 col-md-7 col-lg-6  col-xl-5 d-flex'>
-                <button className='btn btn-success me-3' onClick={()=>navigate('/')}>Home</button>
-                <input 
-                 className='form-control'
-                 type='search'
-                 placeholder='search..'
-                 />
-                 <button className='btn btn-success ms-3' onClick={()=>navigate('/Cart')}>Cart</button>
-            </div>
+    <div className="bg-secondary p-3">
+      <h3>My Projects</h3>
+      <div className="row justify-content-center pt-2 pb-1">
+        <div className="col-sm-12 col-md-7 col-lg-6  col-xl-5 d-flex">
+          <button
+            className="btn btn-success me-3"
+            onClick={() => navigate("/")}
+          >
+            Home
+          </button>
+          <input
+            className="form-control"
+            type="search"
+            placeholder="search.."
+          />
+          <button
+            className="btn btn-success ms-3"
+            onClick={() => navigate("/Cart")}
+          >
+            Cart
+          </button>
         </div>
-   </div>
-  )
+      </div>
+    </div>
+  );
 }
